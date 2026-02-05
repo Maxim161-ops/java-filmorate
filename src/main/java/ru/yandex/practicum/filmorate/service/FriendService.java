45721bfa3828e23165f076cd65be5102a8fd8c83
Maxim161-ops <ddar2508-221B@yandex.ru>
2026-02-05 16:45:52 +0300

    Исправил Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/FriendService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FriendService.java
@@ -4,8 +4,12 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.Collection;
 
 public interface FriendService {
+
     void addFriend(int userId, int friendId);
+
     void removeFriend(int userId, int friendId);
+
     Collection<User> getFriends(int userId);
+    
     Collection<User> getCommonFriends(int userId, int otherId);
 }
