2cf166cb4168a3498c8bb133f32fcd7816d2c0b1
Maxim161-ops <ddar2508-221B@yandex.ru>
2026-02-10 15:51:11 +0300

    Исправил Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/FriendsDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/FriendsDbStorage.java
@@ -23,9 +23,9 @@ public class FriendsDbStorage {
 
     public void removeFriend(int userId, int friendId) {
         String sql = """
-            DELETE FROM friends
-            WHERE (user_id = ? AND friend_id = ?)
-               OR (user_id = ? AND friend_id = ?)
+DELETE FROM friends
+WHERE (user_id = ? AND friend_id = ?)
+   OR (user_id = ? AND friend_id = ?)
 """;
         int rows = jdbc.update(sql, userId, friendId, friendId, userId);
         if (rows == 0) {
