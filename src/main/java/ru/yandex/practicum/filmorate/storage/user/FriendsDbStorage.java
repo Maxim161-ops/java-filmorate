6c5cc2d0c372de801b59951ee3fc5c6ecf6317d4
Maxim161-ops <ddar2508-221B@yandex.ru>
2026-02-11 18:30:59 +0300

    Исправил метод removeFriend

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/FriendsDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/FriendsDbStorage.java
@@ -27,10 +27,7 @@ public class FriendsDbStorage implements FriendsStorage {
 
     @Override
     public int removeFriend(int userId, int friendId) {
-        // Удаляем дружбу в обе стороны
-        int removed1 = jdbc.update("DELETE FROM friends WHERE user_id = ? AND friend_id = ?", userId, friendId);
-        int removed2 = jdbc.update("DELETE FROM friends WHERE user_id = ? AND friend_id = ?", friendId, userId);
-        return removed1 + removed2;
+        return jdbc.update("DELETE FROM friends WHERE user_id = ? AND friend_id = ?", userId, friendId);
     }
 
     @Override
@@ -42,11 +39,11 @@ public class FriendsDbStorage implements FriendsStorage {
     @Override
     public Collection<Integer> getCommonFriends(int userId, int otherId) {
         String sql = """
-            SELECT f1.friend_id
-            FROM friends f1
-            JOIN friends f2 ON f1.friend_id = f2.friend_id
-            WHERE f1.user_id = ? AND f2.user_id = ?
-        """;
+        SELECT f1.friend_id
+        FROM friends f1
+        JOIN friends f2 ON f1.friend_id = f2.friend_id
+        WHERE f1.user_id = ? AND f2.user_id = ?
+    """;
         return jdbc.query(sql, (rs, rowNum) -> rs.getInt("friend_id"), userId, otherId);
     }
 }
